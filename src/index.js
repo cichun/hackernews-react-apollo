8b4ffb48aa34552b9df929f0a642d92b366707bd
Robert Maciaszczyk <r.maciaszczyk@myaic.com>
2022-08-09 16:47:42 +0200

    add base boilerplate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+
+import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from "@apollo/client";
+
+const httpLink = createHttpLink({uri: 'http://localhost:4000'})
+const client = new ApolloClient({link: httpLink, cache: InMemoryCache()})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App/>
+        </ApolloProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
